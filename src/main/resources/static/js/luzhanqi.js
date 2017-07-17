5df9b8b1ba73dd7590e2ad8479df4ef1a7dfe66e
qingshan <xiaqingshan@126.com>
2017-07-17 22:38:41 +0800

    Init fanfango

--- src/main/resources/static/js/luzhanqi.js
+++ src/main/resources/static/js/luzhanqi.js
@@ -52,33 +52,20 @@ var chiziguanxi ={
 var qizixinxi = {};
 
 function initAll() {
-
-
     allRolesList = ["junqi","siling","junzhang","shizhang1","shizhang2","lvzhang1","lvzhang2","tuanzhang1","tuanzhang2","yingzhang1","yingzhang2","lianzhang1","lianzhang2","lianzhang3","paizhang1","paizhang2","paizhang3","gongbing1","gongbing2","gongbing3","dilei1","dilei2","dilei3","zhadan1","zhadan2"];
     redRolesList = ["junqi","siling","junzhang","shizhang1","shizhang2","lvzhang1","lvzhang2","tuanzhang1","tuanzhang2","yingzhang1","yingzhang2","lianzhang1","lianzhang2","lianzhang3","paizhang1","paizhang2","paizhang3","gongbing1","gongbing2","gongbing3","dilei1","dilei2","dilei3","zhadan1","zhadan2"];
     blueRolesList = ["junqi","siling","junzhang","shizhang1","shizhang2","lvzhang1","lvzhang2","tuanzhang1","tuanzhang2","yingzhang1","yingzhang2","lianzhang1","lianzhang2","lianzhang3","paizhang1","paizhang2","paizhang3","gongbing1","gongbing2","gongbing3","dilei1","dilei2","dilei3","zhadan1","zhadan2"];
 
     function huaqizi(hori,vert,hongorlan,role) {
         theNodeHandler = $("#bh"+hori+"v"+vert);
-        theNodeHandler.removeClass("bingzhan");
-        if(hongorlan == "hong"){
-            theNodeHandler.addClass("anqi");
-        }
-        if(hongorlan == "lan"){
-            theNodeHandler.addClass("anqi");
-        }
-
-        // theNodeHandler.text(role);
+        $("#bh"+hori+"v"+vert).find('div[name="qiwei"]').addClass("anqi");
     }
-
 
     notInitList = [
         "0,6","1,6","2,6","3,6","4,6",
         "1,2","3,2","2,3","1,4","3,4",
         "1,8","3,8","2,9","1,10","3,10",
     ];
-
-
 
     for(v=12; v>-1; v--){
         for(h=4; h>-1; h--){
@@ -114,56 +101,83 @@ function initAll() {
     }
 }
 
-//
-initAll();
+
 
 
 var fighting = false;
 var tofightingid = undefined;
-$("div[id^='bh']").click(function(){
-    mm = $("#"+this.id);
+function clearQizi(handlerMM){
+	handlerMM.children(".anqi").remove();
+}
+
+$("li[id^='bh']").click(function(){
+    var toFightId = $("#"+tofightingid).find('div[name="qiwei"]');
+    var thisId =$("#"+this.id).find('div[name="qiwei"]');
     if(fighting){
         if(qizixinxi[this.id].split("-")[0] != qizixinxi[tofightingid].split("-")[0]){
+            //duijia gan
+            console.log("duijia kaigan");
             var gongfang = qizixinxi[tofightingid].split("-")[1];
             var shoufang = qizixinxi[this.id].split("-")[1]
             if(chiziguanxi[gongfang] > chiziguanxi[shoufang ]){
-                $("#"+tofightingid).text("");
-                $("#"+tofightingid).removeClass(qizixinxi[tofightingid].split("-")[0]+"fangqizi");
-                $("#"+this.id).addClass(qizixinxi[tofightingid].split("-")[0]+"fangqizi");
-                $("#"+this.id).removeClass(qizixinxi[this.id].split("-")[0]+"fangqizi");
-                $("#"+this.id).text(qizixinxi[tofightingid].split("-")[1]);
+                console.log("shengduifang tidaizhi");
+                toFightId.text("");
+                toFightId.removeClass(qizixinxi[tofightingid].split("-")[0]+"fangqizi");
+                thisId.addClass(qizixinxi[tofightingid].split("-")[0]+"fangqizi");
+                thisId.removeClass(qizixinxi[this.id].split("-")[0]+"fangqizi");
+                thisId.text(qizixinxi[tofightingid].split("-")[1]);
                 qizixinxi[this.id] = qizixinxi[tofightingid];
                 qizixinxi[tofightingid] = undefined;
                 fighting = false;
                 tofightingid = undefined
             }else if(chiziguanxi[gongfang]== chiziguanxi[shoufang ]){
-                $("#"+tofightingid).removeClass(qizixinxi[tofightingid].split("-")[0]+"fangqizi");
-                $("#"+tofightingid).text("");
-                $("#"+this.id).removeClass(qizixinxi[this.id].split("-")[0]+"fangqizi");
-                $("#"+this.id).text("");
+                console.log("yiyangda pingle")
+                toFightId.removeClass(qizixinxi[tofightingid].split("-")[0]+"fangqizi");
+                toFightId.text("");
+                thisId.removeClass(qizixinxi[this.id].split("-")[0]+"fangqizi");
+                thisId.text("");
                 qizixinxi[this.id] = undefined;
                 qizixinxi[tofightingid] = undefined;
                 fighting = false;
                 tofightingid = undefined
             }else{
+                console.log("ganbuguo meibianhua");
                 fighting = false;
                 tofightingid = undefined
             }
 
         }else if(qizixinxi[this.id].split("-")[0] == qizixinxi[tofightingid].split("-")[0]){
+            //benjia huan
+            console.log("shibenjia huangeqizi caozuo");
             tofightingid = this.id;
         }else{
+            //kongde yiwei
+            console.log("yidong dao ci kongweizhi");
+            toFightId.text("");
+            toFightId.removeClass(qizixinxi[tofightingid].split("-")[0]+"fangqizi");
+            thisId.addClass(qizixinxi[tofightingid].split("-")[0]+"fangqizi");
+            thisId.removeClass(qizixinxi[this.id].split("-")[0]+"fangqizi");
+            thisId.text(qizixinxi[tofightingid].split("-")[1]);
+            qizixinxi[this.id] = qizixinxi[tofightingid];
+            qizixinxi[tofightingid] = undefined;
             fighting = false;
+            tofightingid = undefined
         }
     }else{
-        if(this.className == "anqi"){
-            mm.removeClass("anqi");
-            mm.addClass(qizixinxi[this.id].split("-")[0]+"fangqizi");
-            mm.text(qizixinxi[this.id].split("-")[1]);
+        if($(this).find(".anqi").length ==1 ){
+        	$("#"+this.id).find('div[name="qiwei"]').removeClass("anqi");
+        	$("#"+this.id).find('div[name="qiwei"]').addClass(qizixinxi[this.id].split("-")[0]+"fangqizi");
+        	$("#"+this.id).find('div[name="qiwei"]').text(qizixinxi[this.id].split("-")[1]);
             qizixinxi[this.id] = qizixinxi[this.id].replace("-an","-ming");
-        }else if(this.className.indexOf("fangqizi") > -1){
+        }else if($(this).find(".lanfangqizi").length ==1 || $(this).find(".hongfangqizi").length ==1){
             fighting = true;
             tofightingid = this.id;
         }
     }
+});
+
+$("#refreshBtn").click(function () {
+    initAll();
+    fighting = false;
+    tofightingid = undefined;
 });
